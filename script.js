4a729187038adc015ac9aee3258547efec77d650
DicksonLegend <dicksyn2006@gmail.com>
2025-07-14 23:42:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let currentSpeech = null;
 let currentChatId = null;
 let chatSessions = {};
 
-const API_BASE_URL = 'window.location.origin';
+const API_BASE_URL = 'https://web-production-298a.up.railway.app';
 const STORAGE_KEYS = {
     CHAT_SESSIONS: 'legendai_chat_sessions',
     CURRENT_CHAT: 'legendai_current_chat',
